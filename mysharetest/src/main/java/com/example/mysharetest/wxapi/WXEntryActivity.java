1b22ad7040bd7b09686cbcb4dc7e890b83ccbfbc
hellobenji <863360109@qq.com>
2016-11-12 23:48:55 +0800

    Initial commit

---
+++ mysharetest/src/main/java/com/example/mysharetest/wxapi/WXEntryActivity.java
@@ -0,0 +1,15 @@
+package com.example.mysharetest.wxapi;
+
+import android.os.Bundle;
+
+import com.example.mysharetest.R;
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wxentry);
+    }
+}
